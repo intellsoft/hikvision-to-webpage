9a2460ac98920e430d4741af70aa691ff3f64652
mohammad ali abbaspor <4vip.abbaspor@gmail.com>
2022-11-22 15:05:32 +0330

    Update index.php
    بهینه سازی کد های html

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-
 <?php
+/*
+ * M.Ali.Abbaspor
+ * Desc: show hikvision cctv image camera in web page
+ *
+ * note: only work in hikvision camera with ISAPI function
+ */
 $Camera_IP = "192.168.100.175";
 $Camera_Port = "85";
 $Camera_Username = "user";
@@ -12,10 +17,12 @@ $url = $Camera_Pre_url . $Camera_Username . ":" . $Camera_Password . "@". $Camer
 $current = file_get_contents($url);
 file_put_contents("hh.jpeg",$current);
 
-echo '<img id="myImage" src="hh.jpeg">';
+$camera =  '<img id="myImage" src="hh.jpeg">';
 
 ?>
-
+<html>
+<body>
+<?php echo $camera; ?>
 <script>
 
     var intervalId = window.setInterval(function(){
@@ -27,3 +34,6 @@ echo '<img id="myImage" src="hh.jpeg">';
         document.getElementById("myImage").src = "cctv.php"+ `?v=${new Date().getTime()}`;
     }
 </script>
+
+</body>
+</html>
