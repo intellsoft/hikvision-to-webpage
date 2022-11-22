9d7a7c2fc036caea4ffc74dbcc024ab58bd8d6a4
mohammad ali abbaspor <4vip.abbaspor@gmail.com>
2022-11-22 15:15:09 +0330

    Update cctv.php

--- cctv.php
+++ cctv.php
@@ -1,7 +1,6 @@
 <?php
 require_once ("Config.php");
 
-$url = "http://user:123456qQ@192.168.100.175:85/ISAPI/Streaming/channels/1/picture";
 $current = file_get_contents($url);
 
 header('Content-type: image/jpeg');
